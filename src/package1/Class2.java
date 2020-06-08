fed695dfccba60434ed42b1505a477ec3a2c7500
serdaruz75 <uzunogluserdar@hotmail.com>
2020-06-08 16:42:13 -0400

    i added one

--- src/package1/Class2.java
+++ src/package1/Class2.java
@@ -2,6 +2,7 @@ package package1;
 
 public class Class2 {
     public static void main(String[] args) {
-        System.out.println("woerking");
+        int a=5,b=7;
+        System.out.println(a+b);
     }
 }
